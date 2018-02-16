68f0a707ea040be02d95546b550b7c381842e0f6
Manoj Dayaram <noj@squareup.com>
2018-02-15 19:23:24 -0800

    adding timestamps to prevent re-use of tokens

--- proof_of_work.rb
+++ proof_of_work.rb
@@ -1,24 +1,32 @@
 require 'digest'
+require 'time'
 
 class ProofOfWork
   attr_reader :challenge
   attr_reader :work_factor
+  attr_reader :time_range
 
-  def initialize(challenge, work_factor)
+  # Time range is in seconds.
+  def initialize(challenge:, work_factor:, time_range: 10)
     @challenge = challenge
     @work_factor = work_factor
+    @time_range = time_range
   end
 
   def mint
     i = 0
     begin
       i += 1
-      token = next_token(i)
+      timestamp = Time.now
+      token = make_token(i, timestamp)
     end while(!verify(token))
-    token
+
+    { token: token, timestamp: timestamp }
   end
 
   def verify(token)
+    return false if !time_from_token(token).between?(Time.now - time_range, Time.now)
+
     digest(token).start_with?("0" * work_factor)
   end
 
@@ -28,7 +36,11 @@ class ProofOfWork
 
   private
 
-  def next_token(i)
-    i.to_s
+  def make_token(i, timestamp)
+    i.to_s + "|" + timestamp.to_i.to_s
+  end
+
+  def time_from_token(token)
+    Time.at(token.split("|").last.to_i)
   end
 end
